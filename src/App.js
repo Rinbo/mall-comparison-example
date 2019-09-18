d77478cffce6fb6481ba89ff7f0ac610d650bd18
Robin <robin.borjesson@viametrics.com>
2019-09-18 17:13:20 +0200

    Add index.js, App.js and dummyData.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { malls } from "./dummyData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [commodities, setCommodities] = useState(false);
+  const [categories, setCategories] = useState(false);
+  const [kedjor, setKedjor] = useState(false);
+
+  const renderColumnTitles = () => {
+    return Object.values(malls).map(mall => {
+      return <th colSpan="2">{mall.name}</th>;
+    });
+  };
+
+  const renderMetaData = () => {
+    return Object.values(malls).map(mall => {
+      return (
+        <>
+          <th>Omsättning</th>
+          <th>Utveckling</th>
+        </>
+      );
+    });
+  };
+
+  const renderTableBody = () => {
+    return Object.values(malls).map(mall => {
+      return (
+        <tr>
+          <td>{mall.varugrupper["Beklädnad"].name}</td>
+          <td>{mall.kategorier["Kläder"].revenue}</td>
+          <td>{mall.kedjor["HM"].utveckling}</td>
+        </tr>
+      );
+    });
+  };
+
+  const renderColumnLeftMargin = () => {
+    return (
+      <tr>
+        <td>{commodities ? malls.mobilia.commodname : null}</td>
+        <td>{categories ? malls.mobilia.name : null}</td>
+        <td>{commodities ? malls.mobilia.name : null}</td>
+        <td>None</td>
+        <td>Resets rating to default value</td>
+      </tr>
+    );
+  };
+
+  const renderMallsInColumn = () => {
+    return (
+      <div className="ui container" style={{ marginTop: 100 }}>
+        <table className="ui definition table">
+          <thead>
+            <tr>
+              <th></th>
+              {renderColumnTitles()}
+            </tr>
+
+            <tr>
+              <th></th>
+              {renderMetaData()}
+            </tr>
+          </thead>
+          <tbody>{renderTableBody()}</tbody>
+        </table>
+      </div>
+    );
+  };
+
+  return renderMallsInColumn();
+};
 
 export default App;
---
+++ src/dummyData.js
@@ -0,0 +1,174 @@
+export const malls = {
+  mobilia: {
+    name: "Mobilia",
+    revenue: 1000,
+    utveckling: -3,
+    varugrupper: {
+      begklädnad: {
+        name: "Beklädnad",
+        revenue: 1000,
+        utveckling: -3
+      },
+      dagligvaror: {
+        name: "Dagligvaror",
+        revenue: 1000,
+        utveckling: -3
+      }
+    },
+    kategorier: {
+      klader: {
+        name: "Kläder",
+        revenue: 1000,
+        utveckling: -3
+      },
+      livsmedel: {
+        name: "Livsmedel",
+        revenue: 1000,
+        utveckling: -3
+      }
+    },
+    kedjor: {
+      HM: {
+        name: "HM",
+        revenue: 1000,
+        utveckling: -3,
+        OCR: 1.5
+      },
+      ICAKvantum: {
+        name: "ICA kvantum",
+        revenue: 1000,
+        utveckling: -3,
+        OCR: 5
+      }
+    }
+  },
+  farstaCentrum: {
+    name: "Farsta Centrum",
+    revenue: 1000,
+    utveckling: -3,
+    varugrupper: {
+      begklädnad: {
+        name: "Beklädnad",
+        revenue: 1000,
+        utveckling: -3
+      },
+      dagligvaror: {
+        name: "Dagligvaror",
+        revenue: 1000,
+        utveckling: -3
+      }
+    },
+    kategorier: {
+      klader: {
+        name: "Kläder",
+        revenue: 1000,
+        utveckling: -3
+      },
+      livsmedel: {
+        name: "Livsmedel",
+        revenue: 1000,
+        utveckling: -3
+      }
+    },
+    kedjor: {
+      HM: {
+        name: "HM",
+        revenue: 1000,
+        utveckling: -3,
+        OCR: 1.5
+      },
+      ICAKvantum: {
+        name: "ICA kvantum",
+        revenue: 1000,
+        utveckling: -3,
+        OCR: 5
+      }
+    }
+  },
+  forumGallerian: {
+    name: "Forum Gallerian",
+    revenue: 1000,
+    utveckling: -3,
+    varugrupper: {
+      begklädnad: {
+        name: "Beklädnad",
+        revenue: 1000,
+        utveckling: -3
+      },
+      dagligvaror: {
+        name: "Dagligvaror",
+        revenue: 1000,
+        utveckling: -3
+      }
+    },
+    kategorier: {
+      klader: {
+        name: "Kläder",
+        revenue: 1000,
+        utveckling: -3
+      },
+      livsmedel: {
+        name: "Livsmedel",
+        revenue: 1000,
+        utveckling: -3
+      }
+    },
+    kedjor: {
+      HM: {
+        name: "HM",
+        revenue: 1000,
+        utveckling: -3,
+        OCR: 1.5
+      },
+      ICAKvantum: {
+        name: "ICA kvantum",
+        revenue: 1000,
+        utveckling: -3,
+        OCR: 5
+      }
+    }
+  },
+  sickla: {
+    name: "Sickla",
+    revenue: 1000,
+    utveckling: -3,
+    varugrupper: {
+      begklädnad: {
+        name: "Beklädnad",
+        revenue: 1000,
+        utveckling: -3
+      },
+      dagligvaror: {
+        name: "Dagligvaror",
+        revenue: 1000,
+        utveckling: -3
+      }
+    },
+    kategorier: {
+      klader: {
+        name: "Kläder",
+        revenue: 1000,
+        utveckling: -3
+      },
+      livsmedel: {
+        name: "Livsmedel",
+        revenue: 1000,
+        utveckling: -3
+      }
+    },
+    kedjor: {
+      HM: {
+        name: "HM",
+        revenue: 1000,
+        utveckling: -3,
+        OCR: 1.5
+      },
+      ICAKvantum: {
+        name: "ICA kvantum",
+        revenue: 1000,
+        utveckling: -3,
+        OCR: 5
+      }
+    }
+  }
+};
