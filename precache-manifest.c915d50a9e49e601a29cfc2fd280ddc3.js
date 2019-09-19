68d25c100e31c0b1c12e673a2d2c0351890b127c
Robin <robin.borjesson@viametrics.com>
2019-09-19 09:45:34 +0200

    Updates

---
+++ precache-manifest.c915d50a9e49e601a29cfc2fd280ddc3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b53399df54a95d3da8bf467177fd91b",
+    "url": "/mall-comparison-example/index.html"
+  },
+  {
+    "revision": "8c0f2bd9daa561fcfdaf",
+    "url": "/mall-comparison-example/static/css/2.eaeba0b2.chunk.css"
+  },
+  {
+    "revision": "ba05857c5069f8717b81",
+    "url": "/mall-comparison-example/static/css/main.d3fdcf4d.chunk.css"
+  },
+  {
+    "revision": "8c0f2bd9daa561fcfdaf",
+    "url": "/mall-comparison-example/static/js/2.05e3744b.chunk.js"
+  },
+  {
+    "revision": "ba05857c5069f8717b81",
+    "url": "/mall-comparison-example/static/js/main.df7d1295.chunk.js"
+  },
+  {
+    "revision": "07dcfca1972847ba5df9",
+    "url": "/mall-comparison-example/static/js/runtime~main.af1c3813.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/mall-comparison-example/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/mall-comparison-example/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/mall-comparison-example/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/mall-comparison-example/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/mall-comparison-example/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/mall-comparison-example/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/mall-comparison-example/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/mall-comparison-example/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/mall-comparison-example/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/mall-comparison-example/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/mall-comparison-example/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/mall-comparison-example/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/mall-comparison-example/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/mall-comparison-example/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/mall-comparison-example/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/mall-comparison-example/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
